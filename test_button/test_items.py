905dd331492a51fe456a30212eb65a66ee60d9e5
unknown <ilyabelick@gmail.com>
2020-11-11 23:48:01 +0200

    create conftest and test_items

--- test_button/test_items.py
+++ test_button/test_items.py
@@ -3,3 +3,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_add_to_cart_button(browser):
     browser.get(link)
     assert browser.find_element_by_css_selector("#add_to_basket_form button")
+
