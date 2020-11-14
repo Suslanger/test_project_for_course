be8931ad42d9df81e15025910156a47ef7b022fb
unknown <ilyabelick@gmail.com>
2020-11-14 11:46:52 +0200

    rewrite go to login page

--- module4/test_lesson1/test_main_page.py
+++ module4/test_lesson1/test_main_page.py
@@ -8,6 +8,6 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     page.should_be_login_link()
-    login_page = LoginPage(browser, link)
-    # login_page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
