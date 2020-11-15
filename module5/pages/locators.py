a922a9901bae3b58c91dc94a2a6bf2bea33b5aad
unknown <ilyabelick@gmail.com>
2020-11-15 16:49:53 +0200

    parametrize

---
+++ module5/pages/locators.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, "#add_to_basket_form button")
+    MESSAGE_FORM = (By.CSS_SELECTOR, "#messages")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PRODUCT_NAME_BASKET = (By.CSS_SELECTOR, "#messages div.alert:nth-child(1) strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main p")
+    PRODUCT_PRICE_BASKET = (By.CSS_SELECTOR, "#messages div.alert:nth-child(3) strong")
+
