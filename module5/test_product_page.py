8a2804d5ecb60ecf5cf9f529fffae3c8a2341eee
ilaybelick <ilyabelick@gmail.com>
2021-01-24 18:51:01 +0200

    negative checks

--- module5/test_product_page.py
+++ module5/test_product_page.py
@@ -21,3 +21,22 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_message_form()
     page.should_be_product_name()
     page.should_be_product_price()
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/")
+    page.open()                      # открываем страницу
+    page.add_product_to_basket()
+    page.should_not_be_success_message() #Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+ 
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/")
+    page.open()                      # открываем страницу
+    page.should_not_be_success_message()  #Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+ 
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/")
+    page.open()                      # открываем страницу
+    page.add_product_to_basket()
+    page.should_not_disapear_message() #Проверяем, что нет сообщения об успехе с помощью is_disappeared
