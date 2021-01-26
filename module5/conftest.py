19861ada49028d93d630ee09cf8367b7f493e63c
ilaybelick <ilyabelick@gmail.com>
2021-01-26 22:36:40 +0200

    delete addoption --language

--- module5/conftest.py
+++ module5/conftest.py
@@ -2,9 +2,9 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='en',
-                     help="Choose localization")
+# def pytest_addoption(parser):
+#     parser.addoption('--language', action='store', default='en',
+#                      help="Choose localization")
 
 
 @pytest.fixture(scope="function")
